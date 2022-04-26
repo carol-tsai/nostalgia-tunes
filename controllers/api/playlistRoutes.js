1b46608470b467c66e8d351d7a98759a07d7c7eb
Julian Richardson <jrichar3@yahoo.com>
2022-04-26 13:10:39 -0400

    added routes for playlist, song, and user

---
+++ controllers/api/playlistRoutes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { Playlist } = require('../../models');
+
+// ADD UPDATE 
+
+router.post('/', async (req, res) => {
+  try {
+    const newPlaylist = await Playlist.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPlaylist);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const newPlaylist = await Playlist.update({
+      ...req.body,
+    }, {
+      id: req.params.id
+    });
+
+    res.status(200).json(newPlaylist);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const playlistData = await Playlist.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!playlistData) {
+      res.status(404).json({ message: 'No playlist found!' });
+      return;
+    }
+
+    res.status(200).json(playlistData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ controllers/api/songRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Song } = require('../../models');
+
+// router.post('/:id', async (req, res) => {
+//     try {
+//       const newSong = await Song.create({
+//         ...req.body,
+//       });
+  
+//       res.status(200).json(newSong);
+//     } catch (err) {
+//       res.status(400).json(err);
+//     }
+//   });
+
+
+  module.exports = router;
